103c85dee0084a05d7c17275cef059daf1b90c47
Jack Wu <leopardsaga@gmail.com>
2021-12-16 11:48:24 +0800

    Fix bug of WindowTitle.

--- main.py
+++ main.py
@@ -12,9 +12,9 @@ from mainwindow import Ui_MainWindow
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("Math OCR")
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+        self.setWindowTitle("Math OCR")
         
         self.ui.clearButton.clicked.connect(self.ui.label.clear)
 
